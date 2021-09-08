901a83f4272bdf0b15b65baece940f26eb692bb3
Guilherme Bento <guilherme1550@hotmail.com>
2021-09-08 15:18:12 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -3,7 +3,6 @@ package com.devsuperior.dsvendas.entities;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,7 +19,6 @@ public class Seller {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column()
 	private String name;
 	
 	@OneToMany(mappedBy = "seller")
