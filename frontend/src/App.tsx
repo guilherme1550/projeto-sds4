ce121088ee7945f25e17ec3681cf26ac245ad99f
Guilherme Bento <guilherme1550@hotmail.com>
2021-09-09 11:16:50 -0300

    Group by search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
             <BarChart />
           </div>
           <div className="col-sm-6">
-            <h5 className="text-secondary text-center">Taxa de Sucesso (%)</h5>
+            <h5 className="text-secondary text-center">Todas vendas</h5>
             <DonutChart />
           </div>
         </div>
